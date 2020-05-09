698b9e8cdec032f2bb0407deb11b15fb6f506f29
Philip Kouchner <philipk179@gmail.com>
2020-05-09 11:08:02 -0700

    add REDDIT API

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ $(document).ready(function () {
   // ---------- VARIABLES ----------
 
   var search = "";
-  var title = "";
   var posterURL = "";
   var oldSearch = "";
-
+  // add---------------------------------------
+  var arrReddit;
+  var arrPicture;
+  var data;
+  var toggleStatus;
   // ---------- ON.CLICKS ----------
 
   // submit button
@@ -15,8 +18,12 @@ $(document).ready(function () {
     $("#pastSearches").append(
       `<button class="reSearch btn">${search}</button>`
     );
+
+
+    whiteModeDark(toggleStatus);
+    $("#reddit").html("");
     renderGiphyResults(search);
-    renderRedditResults(search);
+    renderRedditResults(search, arrReddit, arrPicture);
   });
 
   // see more button
@@ -24,8 +31,9 @@ $(document).ready(function () {
   // history
   $(document).on("click", ".reSearch", function () {
     oldSearch = $(this).text();
+    whiteModeDark(toggleStatus);
     renderGiphyResults(oldSearch);
-    renderRedditResults(search);
+    renderRedditResults(oldSearch, arrReddit, arrPicture);
   });
 
   // ---------- FUNCTIONS ----------
@@ -50,23 +58,141 @@ $(document).ready(function () {
         </div>
       </div>`);
       }
+      // adding catch error from GET request
+    }).catch(function (res) {
+
+      $("#myModal").modal();
+
     });
+
     $("#search").val("");
   }
 
-  function renderRedditResults(str) {
+  function renderRedditResults(str, arrReddit, arrReddit) {
+    // ----------------------------------------------------------------------------//
+    var input = myTrim(str);
+    arrReddit = [];
+    arrPicture = [];
+    $("#reddit").html("");
     $.ajax({
+
       type: "GET",
-      url: `https://www.reddit.com/r/${str}/.json`,
-      dataType: "json",
+      url: `https://www.reddit.com/r/${input}/.json`,
+      dataType: "json"
+
     }).then(function (res) {
-      console.log(res);
-      console.log(res.data.children[0].data);
-      $("body").append(`<p>${res.data.children[0].data}</p>`);
+
+      for (var i = 0; i < res.data.children.length; i++) {
+        data = res.data.children[i].data
+        var Reddit = {
+          datalink: 'http://www.reddit.com/' + data.permalink,
+          title: data.title,
+          subreddit: data.subreddit,
+          author: data.author
+        };
+        arrReddit.push(Reddit);
+
+      }
+
+      $.ajax({
+
+        type: "GET",
+        url: `https://www.reddit.com/r/pics/search.json?q=${input}&restrict_sr=on&include_over_18=on&sort=relevance&t=all`,
+        dataType: "json"
+
+
+      }).then(function (res) {
+
+        for (var i = 0; i < res.data.children.length; i++) {
+
+          arrPicture.push(res.data.children[i].data.thumbnail);
+        }
+
+        randomPictureAndComments(arrPicture, arrReddit);
+
+      });
+      // adding catch error from GET request
+    }).catch(function (res) {
+
+      $("#myModal").modal();
+
     });
+
+
   }
+
+  // function deleting white spaeces for Reddit GET request
+  function myTrim(str) {
+
+    return str.replace(/\s/g, '');
+
+  }
+
+  function randomPictureAndComments(arrPicture, arrReddit) {
+
+    for (var i = 0; i < 4; i++) {
+
+      var x = Math.floor((Math.random() * arrPicture.length));
+      var y = Math.floor((Math.random() * arrReddit.length));
+      $("#reddit").append(`
+        <div class="card">
+          <img src="${arrPicture[x]}"
+           class="card-img-top" alt="Picture not found" id="img">
+          <div class="card-body">
+            <p class="card-title font-weight-bold text-dark">Title:</br> ${arrReddit[y].title}</p>
+            <p class="card-text font-weight-bold text-dark">Subreddit:</br>${arrReddit[y].subreddit}</p>
+            <p class="card-text font-weight-bold text-dark">Author:</br> ${arrReddit[y].author}</p>
+            <a href="${arrReddit[y].datalink}" class="font-weight-bold" style="text-decoration:underline ;
+          }">
+            Link Post</p>
+          </div>
+        </div>
+      
+          `)
+
+    }
+
+  }
+
+  function whiteModeDark(toggleStatus) {
+
+    toggleStatus = toggleDisplay.getAttribute("class");
+    if (toggleStatus === "toggle toggleFalse") {
+
+      toggleDisplay.setAttribute("class", " toggle toggleTrue");
+      document.body.setAttribute("class", "darkMode");
+
+
+    } else {
+
+      toggleDisplay.setAttribute("class", "toggle toggleFalse");
+      document.body.setAttribute("class", "lightMode");
+
+    }
+  }
+
+
+  // ------------------------------------------------------------------------------------------------------------------------//
+
 });
 
-//Good job keep it up. Stay coding.
 
-// got it.  Thanks
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+//   type: "GET",
+//   url: `https://www.reddit.com/r/${str}/.json`,
+//   dataType: "json",
+// }).then(function (res) {
+//   console.log(res);
+//   console.log(res.data.children[0].data);
+//   $("body").append(`<p>${res.data.children[0].data}</p>`);
+// });
