b5dfdba76cd85cdfb0a270fe09b7d0dd3d552352
Mat Wilmot <matwilmot90@gmail.com>
2020-05-13 20:02:17 -0700

    changed title

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ $(document).ready(function () {
   var bitlyArr;
   var Bitly;
 
-
-
   // condition checks local storge and start the app
   if (localStorage) {
     tempArray = JSON.parse(window.localStorage.getItem("url"));
@@ -32,7 +30,7 @@ $(document).ready(function () {
     }
   }
 
-  // function submit button 
+  // function submit button
   function submitBtn() {
     $(document).on("click", "#btnSubmit", function (e) {
       e.preventDefault();
@@ -66,7 +64,6 @@ $(document).ready(function () {
     $textBox.remove();
   });
 
-
   // history
   $(document).on("click", ".reSearch", function () {
     oldSearch = $(this).text();
@@ -75,7 +72,7 @@ $(document).ready(function () {
     renderRedditResults(oldSearch, arrReddit, arrPicture);
   });
 
-  // delete local storage 
+  // delete local storage
   $("#deleteBtn").on("click", function (event) {
     event.preventDefault();
     $("#results").html("");
@@ -88,7 +85,6 @@ $(document).ready(function () {
 
   // on click for local storage
   $(document).on("click", ".cardGiphy", function () {
-
     var icon = $(this)[0];
     console.log("hello");
     icon.classList.add("yellow");
@@ -165,7 +161,7 @@ $(document).ready(function () {
     }
   }
 
-  // function for render giphy 
+  // function for render giphy
   function renderGiphyResults(str) {
     $("#results").html("");
 
@@ -195,7 +191,7 @@ $(document).ready(function () {
     $("#search").val("");
   }
 
-  // function for render reddit 
+  // function for render reddit
   function renderRedditResults(str, arrReddit, arrPicture) {
     // ----------------------------------------------------------------------------//
     var input = myTrim(str);
@@ -235,9 +231,9 @@ $(document).ready(function () {
       .catch(function (res) {
         $("#relevantReddit").text(
           res.responseJSON.message +
-          " " +
-          res.responseJSON.cod +
-          " Error from GET Response Reddit"
+            " " +
+            res.responseJSON.cod +
+            " Error from GET Response Reddit"
         );
         $("#seeMoreAt").html("");
       });
@@ -283,6 +279,4 @@ $(document).ready(function () {
       document.body.setAttribute("class", "lightMode");
     }
   }
-
-
 });
